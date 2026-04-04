00e26a507f3b11401ae05655d24a5f7fb80337ee
unknown <hasanur23910@gmail.com>
2026-04-04 20:47:03 +0600

    added cms and contact me and another gallery

---
+++ src/content.config.ts
@@ -0,0 +1,22 @@
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod';
+import { glob } from 'astro/loaders';
+
+const gallery = defineCollection({
+  loader: glob({ base: './src/content/gallery', pattern: "**/*.md" }),
+  schema: z.object({
+    title: z.string(), // category name (e.g. Wedding)
+    featured: z.boolean().optional(),
+    images: z.array(
+      z.object({
+        src: z.string(),
+        alt: z.string().optional(),
+      })
+    ),
+    date: z.coerce.date().optional(),
+  }),
+});
+
+export const collections = {
+  gallery,
+};
